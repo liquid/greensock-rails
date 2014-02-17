bd23b8666b155db7fdc04a47164c12ccb434eacb
Robert Pataki <heartcode@robertpataki.com>
2014-02-17 17:29:03 +0000

    Initial commit
    - Adding latest uncompressed version of GSAP
    - Setting up gem spec file

---
+++ VERSIONS.md
@@ -0,0 +1,5 @@
+# Bundled Versions
+
+| Gem    | Greensock |
+|--------|-----------|
+| 0.1.0  | 1.11.4    |
---
+++ lib/greensock/rails/version.rb
@@ -0,0 +1,6 @@
+module Jquery
+  module Rails
+    VERSION = "0.0.1"
+    GSAP_VERSION = "1.11.4"
+  end
+end
